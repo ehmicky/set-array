836b5fb4e93f218b8d072bd102aacda4dc50b85f
ehmicky <ehmicky@gmail.com>
2022-04-03 19:19:10 +0200

    Fix linting

--- test/main.js
+++ test/main.js
@@ -1,19 +1,14 @@
 import test from 'ava'
-import { each } from 'test-each'
 import setArray from 'set-array'
+import { each } from 'test-each'
 
-each(
-  [
-    [[], {}, []],
-  ],
-  ({ title }, [inputArray, items, outputArray]) => {
-    test(`setArray() output | ${title}`, (t) => {
-      t.deepEqual(setArray(inputArray, items), outputArray)
-    })
-  },
-)
+each([[[], {}, []]], ({ title }, [inputArray, items, outputArray]) => {
+  test(`setArray() output | ${title}`, (t) => {
+    t.deepEqual(setArray(inputArray, items), outputArray)
+  })
+})
 
-each([[true,{}]], ({ title }, args) => {
+each([[true, {}]], ({ title }, args) => {
   test(`setArray() validates input | ${title}`, (t) => {
     t.throws(setArray.bind(undefined, ...args))
   })
